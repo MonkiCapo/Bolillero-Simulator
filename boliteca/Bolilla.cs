44d17c204765b06ba6f8c38f6098f04a722472b0
MonkiCapo <nahojiroElcavernicola@gmail.com>
2026-04-08 18:17:40 -0300

    Comenzando bolillero

---
+++ boliteca/Bolilla.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boliteca
+{
+    public class Bolilla
+    {
+        public int NumeroBolilla;
+
+        public Bolilla(int NumeroBolilla)
+        {
+            this.NumeroBolilla = NumeroBolilla;
+        }
+    }
+}
---
+++ boliteca/Bolillero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boliteca;
+
+public class Bolillero : IBolillero
+{
+    public int CantidadAJugar { get; set; }
+    public List<Bolilla> bolillas { get; set; }
+
+    public Random r = new Random();
+
+    public Bolillero(int CantidadAJugar)
+    {
+        bolillas = new List<Bolilla>();
+        this.CantidadAJugar = CantidadAJugar;
+    }
+
+    public Bolilla obtenerBolilla()
+    {
+        int indice = r.Next(0, bolillas.Count);
+        Bolilla bolillaSeleccionada = bolillas[indice];
+
+        bolillas.RemoveAt(indice);
+
+        return bolillaSeleccionada;
+    }
+}
